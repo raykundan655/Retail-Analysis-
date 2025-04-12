33fff2e376b5150826fb792f1dfb878d355c5f1e
Kundan Ray <149966879+raykundan655@users.noreply.github.com>
2025-04-12 16:00:40 +0530

    cleaning sampleStore1.py

--- sampleStore1.py
+++ sampleStore1.py
@@ -11,3 +11,27 @@ return_df=pd.read_excel(file_path,sheet_name="Returns")
 # print(df)
 print(df.head())
 print(df.info())
+
+# cleaning
+print(df.isnull().sum())
+print(df["Order ID"].duplicated().sum())
+df.drop_duplicates(subset="Order ID",keep="first",inplace=True)
+print(df["Order ID"].duplicated().sum())
+
+df["Order Date"]=pd.to_datetime(df["Order Date"])
+df["Ship Date"]=pd.to_datetime(df["Ship Date"])
+
+print(df.columns)
+numeric =["Quantity","Discount","Profit","Sales"]
+df[numeric]=df[numeric].apply(pd.to_numeric)
+
+df1=df[df["Profit"]>=0].copy()
+print(df1)
+
+# merging two sheet
+df1=pd.merge(df1,return_df,on="Order ID",how="left")
+print(df1)
+df1["Returned"].replace(np.nan,"No",inplace=True)
+print(df1)
+
+print(df1.columns)
